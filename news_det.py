5a358d779044cd7f55b2ca3da946fadc933ee519
Nayan Ghimire <152177988+NayanGhimire@users.noreply.github.com>
2025-11-12 12:32:42 +0545

    Update news_det.py

--- news_det.py
+++ news_det.py
@@ -50,7 +50,7 @@ def train_model():
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
     )
-    print("🔢 Vectorizing text...")
+    print("Vectorizing text...")
     vectorizer = TfidfVectorizer(
         stop_words='english',
         max_df=0.7,
